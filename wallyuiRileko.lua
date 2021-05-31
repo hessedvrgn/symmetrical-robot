ae9c4e0bdf9a47d61f1677a45d184adea60f5252
hessedvrgn <82619772+hessedvrgn@users.noreply.github.com>
2021-05-31 12:03:43 +0800

    Changed some stuff..

--- wallyuiRileko.lua
+++ wallyuiRileko.lua
@@ -1,9 +1,56 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kiwi-i/wallys-ui-fork/master/lib.lua", true))()
 local Window = library:CreateWindow('Those Who Remain')
+local Window2 = library:CreateWindow('Otherssss')
 
-local autoon  = Window:Button("Autofarm On", function()
-   _G.toggle = true --or fasle
-while _G.toggle do
+Window:Section('Ammo') -- Ammo Section
+
+local ammoon  = Window:Button("AutoAmmo On", function()
+   _G.AutoAmmo = true --or fasle
+while _G.AutoAmmo do
+wait() --time between running
+for i, v in pairs (game:GetService("Workspace").World.Objectives:GetChildren()) do
+				if v:IsA("Model") and v.Name == "Ammo" and game.workspace.World.Objectives.Ammo:FindFirstChild("Progress") then repeat
+				    while true do
+					game.workspace.Rileko.HumanoidRootPart.CFrame = game.workspace.World.Objectives.Ammo.Progress.CFrame * CFrame.new(0, -8, 0)
+					wait()
+					end
+				until game.workspace.World.Objectives.Ammo:FindFirstChild("Progress") ~= nil
+end
+end
+end
+end)
+
+local ammooff  = Window:Button("AutoAmmo Off", function()
+   _G.AutoAmmo = false --or fasle
+end)
+
+Window:Section('Medical') -- Medical Section
+
+local medicon  = Window:Button("AutoMedic On", function()
+   _G.automedic = true --or fasle
+while _G.automedic do
+wait() --time between running
+for i, v in pairs (game:GetService("Workspace").World.Objectives:GetChildren()) do
+				if v:IsA("Model") and v.Name == "Medical" and game.workspace.World.Objectives.Medical:FindFirstChild("Progress") then repeat
+				    while true do
+					game.workspace.Rileko.HumanoidRootPart.CFrame = game.workspace.World.Objectives.Medical.Progress.CFrame * CFrame.new(0, -8, 0)
+					wait()
+					end
+				until game.workspace.World.Objectives.Medical:FindFirstChild("Progress") ~= nil
+end
+end
+end
+end)
+
+local medicoff  = Window:Button("AutoMedic Off", function()
+   _G.automedic = false --or fasle
+end)
+
+Window:Section('Radio') -- Radio Section
+
+local radioon  = Window:Button("AutoRadio On", function()
+   _G.autoradio = true --or fasle
+while _G.autoradio do
 wait() --time between running
 for i, v in pairs (game:GetService("Workspace").World.Objectives:GetChildren()) do
 				if v:IsA("Model") and v.Name == "Radio" and game.workspace.World.Objectives.Radio:FindFirstChild("Progress") then repeat
@@ -12,65 +59,28 @@ for i, v in pairs (game:GetService("Workspace").World.Objectives:GetChildren()) 
 					wait()
 					end
 				until game.workspace.World.Objectives.Radio:FindFirstChild("Progress") ~= nil
-				if game.workspace.World.Objectives.Radio:FindFirstChild("Progress") == nil then
-				    for i, v in pairs (game:GetService("Workspace").World.Objectives:GetChildren()) do
-				    if v:IsA("Model") and v.Name == "Medical" and game.workspace.World.Objectives.Medical:FindFirstChild("Progress") then repeat
-				    while true do
-					game.workspace.Rileko.HumanoidRootPart.CFrame = game.workspace.World.Objectives.Medical.Progress.CFrame * CFrame.new(0, -8, 0)
-					wait()
-				    end
-				    until game.workspace.World.Objectives.Medical:FindFirstChild("Progress") ~= nil
-				    if game.workspace.World.Objectives.Medical:FindFirstChild("Progress") == nil then
-				        for i, v in pairs (game:GetService("Workspace").World.Objectives:GetChildren()) do
-				    if v:IsA("Model") and v.Name == "Ammo" and game.workspace.World.Objectives.Ammo:FindFirstChild("Progress") then repeat
-				    while true do
-					game.workspace.Rileko.HumanoidRootPart.CFrame = game.workspace.World.Objectives.Ammo.Progress.CFrame * CFrame.new(0, -8, 0)
-					wait()
-				    end
-				    until game.workspace.World.Objectives.Ammo:FindFirstChild("Progress") ~= nil
-				    if game.workspace.World.Objectives.Ammo:FindFirstChild("Progress") == nil then
-				        for i, v in pairs (game:GetService("Workspace").Entities.Objectives:GetChildren()) do
-				    if v:IsA("Model") and v.Name == "EscortChar" and game.workspace.Entities.Objectives.EscortChar:FindFirstChild("HumanoidRootPart") then repeat
-				    while true do
-					game.workspace.Rileko.HumanoidRootPart.CFrame = game.workspace.Entities.Objectives.EscortChar.HumanoidRootPart.CFrame * CFrame.new(0, -8, 0)
-					wait()
-				    end
-				    until game.workspace.Entities.Objectives.EscortChar:FindFirstChild("HumanoidRootPart") ~= nil
-				    if game.workspace.Entities.Objectives.EscortChar:FindFirstChild("HumanoidRootPart") == nil then
-				        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(293,-1438,292)
-				end
-end
-end
-end
-end
-end
-end
-end
-end
-end
 end
 end
 end
 end)
 
-local autooff  = Window:Button("Autofarm Off", function()
-   _G.toggle = false --or fasle
+local radiooff  = Window:Button("AutoRadio Off", function()
+   _G.autoradio = false --or fasle
 end)
+
+Window:Section('Escort') -- Escort Section
 
 local escorton = Window:Button("AutoEscort On", function()
     _G.escort = true --or fasle
 while _G.escort do
 wait() --time between running
 for i,v in pairs (game:GetService("Workspace").Entities.Objectives:GetDescendants()) do
-    if v:IsA("Model") and v.Name == "EscortChar" then repeat
+    if v:IsA("Model") and v.Name == "EscortChar" and game.workspace.Entities.Objectives.EscortChar:FindFirstChild("HumanoidRootPart") then repeat
         while true do
         game.workspace.Rileko.HumanoidRootPart.CFrame = game.workspace.Entities.Objectives.EscortChar.HumanoidRootPart.CFrame * CFrame.new(0,-8,0)
         wait()
         end
-    until game.workspace.Entities.Objectives.EscortChar:FindFirstChild("HumanoidRootPart") ~= nil
-    if game.workspace.Entities.Objectives.EscortChar:FindFirstChild("HumanoidRootPart") ~= nil then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(293,-1438,292)
-end
+        until game.workspace.Entities.Objectives.EscortChar:FindFirstChild("HumanoidRootPart") == nil
 end
 end
 end
@@ -80,19 +90,19 @@ local escortoff = Window:Button("AutoEscort Off", function()
     _G.escort = false --or fasle
 end)
 
-local dtpon  = Window:Button("TPB On", function()
+local dtpon  = Window2:Button("TPB On", function()
   _G.dtp = true --or fasle
 while _G.dtp do
-wait(10) --time between running
+wait(5) --time between running
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(293,-1438,292)
 end
 end)
 
-local dtpoff  = Window:Button("TPB Off", function()
+local dtpoff  = Window2:Button("TPB Off", function()
   _G.dtp = false --or fasle
 end)
 
-local airwalkon = Window:Button("Airwalk On", function()
+local airwalkon = Window2:Button("Airwalk On", function()
     _G.airwalk = true --or fasle
 while _G.airwalk do
 wait() --time between running
@@ -130,6 +140,6 @@ end
 end
 end)
 
-local airwalkoff = Window:Button("Airwalk Off", function()
+local airwalkoff = Window2:Button("Airwalk Off", function()
     _G.airwalk = false --or fasle
 end)
